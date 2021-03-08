dd37349349cfa3a226abde9bbddad4f6a828fcfd
Geoffrey Otieno <50849936+gootieno@users.noreply.github.com>
2021-03-08 12:14:49 -0800

    changed directory names and building an express backend

--- frontend/src/Components/Pages/LandingPage.jsx
+++ frontend/src/Components/Pages/LandingPage.jsx
@@ -1,22 +1,6 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import { useHistory } from "react-router-dom";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import { NavBar } from "./Styles/landingpage";
 
 const LandingPage = () => {
   const history = useHistory();
@@ -24,20 +8,11 @@ const LandingPage = () => {
   const handleLogin = () => {
     return history.push("/login");
   };
-  const classes = useStyles();
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            theBluePrint
-          </Typography>
-          <Button onClick={handleLogin} color="inherit">
-            Login ICON
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <NavBar>
+      <h1>title here</h1>
+      <button login={handleLogin}>Login Icon</button>
+    </NavBar>
   );
 };
 
---
+++ frontend/src/Components/Pages/Styles/landingpage.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LoginButton = styled.button``;
+
+export const NavBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+  flex-grow: 1;
+`;
