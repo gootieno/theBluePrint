28f9f519f07e75298ad84e83ea85c9204a56ed33
gootieno <geoff.otieno7@gmail.com>
2021-03-30 01:00:41 -0700

    finsihed user authentication and authentication routes. Need to test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,24 @@ const apiRouter = require('./api')
 
 router.use('/api', apiRouter)
 
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler')
+const { setTokenCookie } = require('../../utils/auth.js')
+const { User } = require('../../db/models')
+router.get(
+	'/set-token-cookie',
+	asyncHandler(async (req, res) => {
+		const user = await User.findOne({
+			where: {
+				username: 'Demo-lition',
+			},
+		})
+		setTokenCookie(res, user)
+		return res.json({ user })
+	})
+)
+
+//test route for xsrf token
 router.get('/hello/world', (req, res) => {
 	res.cookie('XSRF-TOKEN', req.csrfToken())
 	res.send('Hello World')
