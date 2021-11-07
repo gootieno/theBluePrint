ebdee096c40e94fc520185cd103b94fdcc2efd39
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-11-07 04:45:08 -0800

    styling the landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -9,7 +9,37 @@ import NavBar from "../NavBar";
 import "./landingpage.css";
 
 const LandingPage = () => {
-  return <div id="landing-page" className="landing-page-container"></div>;
+  return (
+    <>
+      <div id="landing-page" className="landing-page-container">
+        <div id="landing-page-message-container" className="landing-page-text">
+          <div id="landing-page-message">
+            <h2>A Place Where You Can </h2>
+            <h2 id="landing-page-revolving-text">Design</h2>
+          </div>
+          <div id="registration-container">
+            <h2 className="registration-text">
+              Don't have an account yet? Join us free today!
+            </h2>
+            <h2 className="registration-text">
+              Browsing around? Try the demo account!
+            </h2>
+            <h3 id="registration-signup" className="registration-text">
+              create an account
+            </h3>
+            <h3
+              type="button"
+              id="registration-demo"
+              className="registration-text"
+            >
+              Demo
+            </h3>
+          </div>
+        </div>
+        <div id="landing-page-footer">dev icons here</div>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
