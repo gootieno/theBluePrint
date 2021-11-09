043bd7bfea39347bf2047adb5ff6d8f16a059917
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-11-08 19:35:30 -0800

    fixed signup page styling issue using flexbox

--- frontend/src/components/SignUpPage/index.js
+++ frontend/src/components/SignUpPage/index.js
@@ -11,41 +11,49 @@ function SignUpPage() {
     password: "",
     confirmPassword: "",
   });
+
   const history = useHistory();
+
   return (
-    <div id="signup-form-page-container">
-      <form id="signup-form-container">
-        <input
-          className="signup-page-terms-content"
-          id="signup-username"
-          type="text"
-          placeholder="username"
-        />
-        <input
-          className="signup-page-terms-content"
-          id="signup-email"
-          type="email"
-          placeholder="email"
-        />
-        <input
-          className="signup-page-terms-content"
-          id="signup-password"
-          type="password"
-          placeholder="password"
-        />
-        <input
-          id="signup-confirm-password"
-          type="password"
-          placeholder="confirm password"
-          className="signup-page-terms-content"
-        />
-      </form>
-      <div id="signup-form-terms-and-conditions">
-        <h3 id="signup-page-terms-and-conditions">Terms and conditions</h3>
-        <p id="signup-page-terms-body"> Live, Laugh, Love</p>
-        <input id="signup-page-terms-confirm" type="checkbox" />
+    <>
+      <h1 id="signup-form-page-title">It all starts here!</h1>
+      <div id="signup-form-page-container">
+        <form id="signup-form-container">
+          <input
+            className="signup-page-terms-content"
+            id="signup-username"
+            type="text"
+            placeholder="username"
+          />
+          <input
+            className="signup-page-terms-content"
+            id="signup-email"
+            type="email"
+            placeholder="email"
+          />
+          <input
+            className="signup-page-terms-content"
+            id="signup-password"
+            type="password"
+            placeholder="password"
+          />
+          <input
+            id="signup-confirm-password"
+            type="password"
+            placeholder="confirm password"
+            className="signup-page-terms-content"
+          />
+        </form>
+        <div id="signup-form-terms-and-conditions-container">
+          <h2 id="signup-page-terms-and-conditions">Terms and conditions</h2>
+          <h3 id="signup-page-terms-body"> Live, Laugh, Love</h3>
+          <span id="signup-page-confirm-container">
+            <input id="signup-page-terms-confirm" type="checkbox" />
+            <span>Agree?</span>
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
