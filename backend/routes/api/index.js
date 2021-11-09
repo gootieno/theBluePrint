bb6a3e38ae53cc7cfc239d71a3ac0c12f59190c2
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-11-09 00:12:34 -0800

    cusers can now login and signup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,19 +1,15 @@
-const router = require('express').Router()
-const { restoreUser } = require('../../utils/auth.js')
+const router = require("express").Router();
+const { restoreUser } = require("../../utils/auth.js");
 
-const sessionRouter = require('./session.js')
-const usersRouter = require('./users.js')
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 
-router.use('/session', sessionRouter)
+router.use("/session", sessionRouter);
 
-router.use('/users', usersRouter)
+router.use("/users", usersRouter);
 
-router.get('/restore-user', restoreUser, (req, res) => {
-	return res.json(req.user)
-})
+router.get("/restore-user", restoreUser, (req, res) => {
+  return res.json(req.user);
+});
 
-router.post('/test', (req, res) => {
-	res.json({ requestBody: req.body })
-})
-
-module.exports = router
+module.exports = router;
