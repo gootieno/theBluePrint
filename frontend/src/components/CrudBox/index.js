766f5dd2135eace9c1f80e63918351ccccae56dc
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-11-15 11:16:14 -0800

    created blueprint specs component and a crud box component for handling crud on /garage.

---
+++ frontend/src/components/CrudBox/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./crudbox.css";
+
+const CrudBox = () => {
+  return (
+    <div>
+      <div id="crudbox-title">work bench</div>
+      <div id="crudbox-container">
+        <div id="crudbox-body">some crud box stuff</div>
+      </div>
+    </div>
+  );
+};
+
+export default CrudBox;
