b8a003a315d9b6959d4faa65091170fb3eabd43f
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-11-21 01:12:51 -0800

    created api route for querying users garage

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,10 +3,12 @@ const { restoreUser } = require("../../utils/auth.js");
 
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
+const garageRouter = require("./garage.js");
 
 router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
+router.use("/garage", garageRouter);
 
 router.get("/restore-user", restoreUser, (req, res) => {
   return res.json(req.user);
