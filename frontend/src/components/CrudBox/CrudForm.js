7e6717d2917f3f28499ab01bf708027f53e75675
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2022-01-21 23:35:39 -0800

    fixed input length when taking in user input. Added file input for images

--- frontend/src/components/CrudBox/CrudForm.js
+++ frontend/src/components/CrudBox/CrudForm.js
@@ -3,6 +3,7 @@ import { dynamicFetch } from "../../redux/dynamicFetch";
 
 const CrudForm = ({ action, handleInputRef, inputRef, routeObject }) => {
   const [inputAction, setInputAction] = useState("");
+
   const { name } = routeObject;
 
   const handleInputAction = (event) => {
@@ -20,39 +21,42 @@ const CrudForm = ({ action, handleInputRef, inputRef, routeObject }) => {
     dynamicFetch({ payload, inputAction });
   };
 
-  return (
-    <div className="crud-actions-input crud-actions">
-      <form type="submit" onSubmit={handleSubmit}>
-        <input
-          ref={inputRef}
-          id="text-box-input"
-          type="text"
-          value={inputAction}
-          autoComplete="off"
-          placeholder={
-            action === "edit"
-              ? ` ${action.toUpperCase()} ${name.toUpperCase()}`
-              : ` ENTER NEW NAME`
-          }
-          onChange={handleInputAction}
-          className={
-            inputAction.length > 0
-              ? "crud-input crud-actions active"
-              : "crud-input crud-actions focus"
-          }
-        />
-        {inputAction.length > 0 && (
-          <span
-            id="input-action-cancel"
-            className="text-inputs-cancel crud-actions"
-            onClick={handleInputCancel}
-          >
-            x
-          </span>
-        )}
-      </form>
-    </div>
+  const blueprintForm = <></>;
+
+  const defaultForm = (
+    <form id="input-field-form" type="submit" onSubmit={handleSubmit}>
+      <input id="blueprint-image" type="file" />
+      <input
+        ref={inputRef}
+        id="text-box-input"
+        type="text"
+        value={inputAction}
+        autoComplete="off"
+        placeholder={
+          action === "edit"
+            ? ` ${action.toUpperCase()} ${name.toUpperCase()}`
+            : ` ENTER NEW NAME`
+        }
+        onChange={handleInputAction}
+        className={
+          inputAction.length > 0
+            ? "crud-input crud-actions active"
+            : "crud-input crud-actions focus"
+        }
+      />
+      {inputAction.length > 0 && (
+        <span
+          id="input-action-cancel"
+          className="text-inputs-cancel crud-actions"
+          onClick={handleInputCancel}
+        >
+          x
+        </span>
+      )}
+    </form>
   );
+
+  return <div className="crud-actions-input crud-actions">{defaultForm}</div>;
 };
 
 export default CrudForm;
--- frontend/src/redux/dynamicFetch.js
+++ frontend/src/redux/dynamicFetch.js
@@ -1,8 +1,11 @@
 import { csrfFetch } from "./csrf";
 
 export const dynamicFetch = async ({ payload }) => {
-  const { routeObject, data, method } = payload;
-  const { route, id } = routeObject;
+  const {
+    routeObject: { route, id },
+    data,
+    method,
+  } = payload;
 
   if (method === "create") {
     const response = await csrfFetch(`/api/${route}`, {
@@ -10,18 +13,21 @@ export const dynamicFetch = async ({ payload }) => {
       contentType: "application/json",
       body: JSON.stringify(data),
     });
+    if (response.ok) return { route, response };
   } else if (method === "edit" && id) {
     const response = await csrfFetch(`/api/${route}/${id}`, {
       method: "PUT",
       contentType: "application/json",
       body: JSON.stringify(data),
     });
+    if (response.ok) return { route, response };
   } else if (method === "delete" && id) {
     const response = await csrfFetch(`/api/${route}/${id}`, {
       method: "DELETE",
       contentType: "application/json",
       body: JSON.stringify(data),
     });
+    if (response.ok) return { route, response };
   } else {
     console.log("does this look right? ", `fetching to /api/${route}/${id}`);
   }
