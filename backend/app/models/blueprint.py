b0de787c1f6565a558ed53a5c746f24908cd3ce2
gootieno <geoff.otieno7@gmail.com>
2022-11-29 15:08:02 -0800

    organizing backend

--- backend/app/models/blueprint.py
+++ backend/app/models/blueprint.py
@@ -10,12 +10,20 @@ class Blueprint(db.Model):
     garage_id = db.Column(db.Integer, db.ForeignKey(
         'garages.id'), nullable=False)
 
-    categories = db.relationship("Category", backref='blueprints')
+    categories = db.relationship("Category", backref='blueprints',
+                                 lazy='joined')
 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
+            'imageUrl': self.image_url
+        }
+
+    def eager_load(self):
+        return {
+            'id': self.id,
+            'name': self.name,
             'imageUrl': self.image_url,
             'categories': [category.to_dict() for category in self.categories]
         }
