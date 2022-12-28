276dd372d3ac8128071c0b48f0349f73c16a46fa
gootieno <geoff.otieno7@gmail.com>
2022-12-28 14:39:53 -0800

    updated project route created

--- backend/app/api/project_routes.py
+++ backend/app/api/project_routes.py
@@ -20,3 +20,28 @@ def create_project():
 
     return jsonify(project.to_dict())
 
+
+# update project
+@project_routes.route("/<int:id>", methods=["PUT", "PATCH"])
+def update_project(id):
+    data = request.json
+
+    updated_name = data["name"]
+    completed = data["completed"]
+    category_id = data["categoryId"]
+
+    project = Project.query.filter_by(id=id, category_id=category_id).first()
+
+    if (updated_name):
+        project.name = updated_name
+
+    if (completed == True or completed == False):
+        project.completed = completed
+
+    if (category_id):
+        project.category_id = category_id
+        
+    db.session.add(project)
+    db.session.commit()
+    
+    return jsonify({'updated_project': project.to_dict()})
