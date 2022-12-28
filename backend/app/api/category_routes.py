6140ac5cdedc2b7c15d06e0ab0c4cb9d2bb1ebf0
gootieno <geoff.otieno7@gmail.com>
2022-12-28 14:10:52 -0800

    put and patch route for categories

--- backend/app/api/category_routes.py
+++ backend/app/api/category_routes.py
@@ -15,4 +15,26 @@ def create_category():
     db.session.add(category)
     db.session.commit()
 
-    return jsonify({"category": category})
+    return jsonify(category.to_dict())
+
+
+# update blueprint category
+@category_routes.route("/<int:id>", methods=["PUT", "PATCH"])
+def update_category(id):
+    data = request.json
+
+    blueprint_id = data["blueprintId"]
+    updated_name = data["name"]
+
+    category = Category.query.get(id)
+
+    if category:
+        if blueprint_id:
+            category.blueprint_id = blueprint_id
+
+        if updated_name:
+            category.name = updated_name
+
+        db.session.commit()
+
+    return jsonify(category.to_dict())
