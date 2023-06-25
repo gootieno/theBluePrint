780cfbf79b03e71d300f7d1021105a2b0b135010
gootieno <geoff.otieno7@gmail.com>
2023-06-24 17:34:33 -0700

    successfully logging in a user

--- backend/app/api/auth_routes.py
+++ backend/app/api/auth_routes.py
@@ -1,8 +1,5 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import set_access_cookies
-from flask_jwt_extended import jwt_required
 from app.models import User
 
 auth_routes = Blueprint('auth', __name__)
@@ -18,12 +15,12 @@ def login():
     
     user = User.query.filter(User.email == email).first()
    
+    print('user password ====================> ', user.check_password(password))
     if user is None:
         return jsonify({'message': "No user with the provided email address"}), 401
         
-    if user and not user.check_password(password):
-        return {"message": "The provided password is incorrect"}
+    # if user is not None and not user.check_password(password):
+    #     return {"message": "The provided password is incorrect"}
     if user and user.check_password(password):
         access_token = create_access_token(identity=email)
-        set_access_cookies(access_token)
         return jsonify(access_token=access_token)
--- backend/app/seeds/users.py
+++ backend/app/seeds/users.py
@@ -5,20 +5,23 @@ from flask_bcrypt import generate_password_hash
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Han', email='demo@aa.io', password=hash_password('password'))
+        username='Han', email='demo@aa.io',
+        hashed_password=hash_password('password'))
     john = User(
-        username='JohnDoe', email='john@aa.io', password=hash_password('password'))
+        username='JohnDoe', email='john@aa.io',
+        hashed_password=hash_password('password'))
     jane = User(
-        username='JaneDoe', email='jane@aa.io', password=hash_password('password'))
+        username='JaneDoe', email='jane@aa.io',
+        hashed_password=hash_password('password'))
 
     db.session.add(demo)
     db.session.add(john)
     db.session.add(jane)
 
     db.session.commit()
-
+    
 def hash_password(password):
-    hashed_password = generate_password_hash(password, 10).decode('utf-8')
+    hashed_password = generate_password_hash(password, 10)
     return hashed_password
 # Uses a raw SQL query to TRUNCATE the users table.
 # SQLAlchemy doesn't have a built in function to do this
