6f81f9849dfabee1f154bff2a73d9d6d4922e322
gootieno <geoff.otieno7@gmail.com>
2023-07-17 17:11:27 -0700

    styled spec component

--- frontend/src/Components/Spec/index.js
+++ frontend/src/Components/Spec/index.js
@@ -1,18 +1,23 @@
 import { useSelector } from "react-redux";
+import "./spec.css";
 
 const Spec = ({ categoryId }) => {
   const specs = useSelector((state) =>
     Object.values(state.specs).filter((spec) => spec.categoryId === categoryId)
   );
+  
   return (
-    <div id="specs-container">
-      {specs &&
-        specs.map((spec) => (
-          <span id={spec.id} className="specs">
-            {spec.name}
-          </span>
-        ))}
-    </div>
+    <>
+      <h2 id="specs-heading">Specs</h2>
+      <ul id="specs-container">
+        {specs &&
+          specs.map((spec) => (
+            <li id={spec.id} className="specs">
+              {spec.name}
+            </li>
+          ))}
+      </ul>
+    </>
   );
 };
 
